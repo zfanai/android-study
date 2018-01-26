d92cbbb13bdbaa9e1b5719a8ecbad335e9678cfe
zhoufan <zf_sch@126.com>
2018-01-26 14:18:45 +0800

    add a dsl demo proj

--- MyApplication4/app/src/main/java/com/example/translucentbarstest/FirstActivity.java
+++ MyApplication4/app/src/main/java/com/example/translucentbarstest/FirstActivity.java
@@ -30,9 +30,9 @@ public class FirstActivity extends AppCompatActivity{
     private void initState() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             //透明状态栏
-           //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+           getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             //透明导航栏
-            //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         }
     }
 
